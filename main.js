18ef3012c54c7df77d3015cdb2a03b2fd5d0d3b6
Kaomymy <117478743+Kaomymy@users.noreply.github.com>
2023-06-16 15:35:33 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function preLoad() {
+    function setUp() {
+        canvas = createCanvas (640 , 480);
+        canvas.position (110 , 150);
+        video = createCapture (VIDEO);
+        video.hide ();
+        tint_color = "";
+    }
+    function draw () {
+image (video , 0, 0, 0, 640, 480);
+tint (tint_color);
+    }
+function take_snapshot (){
+    save ('student_name.png')
+}
+function filter_tint() 
+{ 
+    tint_color = document.getElementById("color_name").value; 
+}
+}
